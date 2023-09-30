044df92d5dfee259db02e61fd6e64708a755bd99
Ashraf Emon <ashraf.emon143@gmail.com>
2023-09-30 13:51:27 +0600

    Social Gateway api routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,10 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 use Leafwrap\SocialConnects\Http\Controllers\SocialConnectController;
+use Leafwrap\SocialConnects\Http\Controllers\SocialGatewayController;
 
-Route::middleware('api')->prefix('api/v1/social-connects')->group(function () {
-    Route::post('auth-request', [SocialConnectController::class, 'socialCall']);
-    Route::post('auth-user', [SocialConnectController::class, 'socialUser']);
+Route::middleware('api')->prefix('api/v1')->group(function () {
+    Route::apiResource('social-gateways', SocialGatewayController::class)->except(['create', 'edit']);
+
+    Route::prefix('social-connects')->group(function () {
+        Route::post('auth-request', [SocialConnectController::class, 'socialCall']);
+        Route::post('auth-user', [SocialConnectController::class, 'socialUser']);
+    });
 });
 
 Route::view('social-connects/redirect', 'social-connects::pages.social-connect-redirect');
